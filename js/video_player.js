d580a6faa00384bace2f9acfd0a809936ca52780
LShere <384757457@qq.com>
2019-11-01 02:18:23 +0800

    Site updated: 2019-11-01 02:18:21

---
+++ js/video_player.js
@@ -0,0 +1,7 @@
+// window.onload = function(){
+   $(function(){
+    $('#video_player').contents().find('.player').css({'border-radius': '30px', 'overflow': 'hidden'});
+    console.log('222')
+   }) 
+    console.log("1111");
+// }

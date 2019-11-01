e9cf97e12f5f25ccf7357573c837f6ea6b260499
LShere <384757457@qq.com>
2019-11-01 13:27:38 +0800

    Site updated: 2019-11-01 13:27:35

--- js/video_player.js
+++ js/video_player.js
@@ -1,7 +1,30 @@
 // window.onload = function(){
-   $(function(){
-    $('#video_player').contents().find('.player').css({'border-radius': '30px', 'overflow': 'hidden'});
-    console.log('222')
-   }) 
-    console.log("1111");
+//    $(function(){
+    // console.log('222')
+//    }) 
+    // console.log("1111");
 // }
+
+// playeronload = function(){
+//     // $('#video_player').contents().find('.player').css({'border-radius': '30px', 'overflow': 'hidden'});
+//     console.log(window.frames[0].document);
+//     //跨域iframe无法修改样式
+// }
+window.onload = function(){
+    let toggle = false;
+    const btn = $('.player-btn');
+    const box = $('.player-box');
+    btn.click(function(){
+        if(!toggle){
+            toggle = true;
+            btn.addClass('btn-active');
+            box.addClass('btn-active');
+        }else{
+            toggle = false;
+            btn.removeClass('btn-active');
+            box.removeClass('btn-active');
+        }
+    })
+      
+    
+}
